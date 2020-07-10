91935516e783be7fef75d99856762004a505f2da
dom <crhicko@protonmail.com>
2020-07-10 17:02:32 -0400

    initial stuff

---
+++ Assets/Resources/Scripts/Unit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Unit", menuName = "Unit", order = 0)]
+public class Unit : ScriptableObject
+{
+    public int maxhp;
+    public int hp;
+    public string _name;
+
+    public int controlCount;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
